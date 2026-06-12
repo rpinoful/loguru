21c0dcd5aee33355c26e9145d65851aac626e635
rpinoful <rapsful@gmail.com>
2026-06-12 15:21:01 -0300

    Adding bassic loggin command using loguru

---
+++ hello.py
@@ -0,0 +1,15 @@
+from loguru import logger
+
+
+
+# classic message - debug
+logger.debug('debug message')
+
+
+
+# log - for info message
+logger.info('info message')
+
+# log - for error
+logger.error('ERROR MESSAGE')
+
