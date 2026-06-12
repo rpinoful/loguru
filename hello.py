088a86fc8d697eead3ebb30db71752c4e8872ff7
rpinoful <rapsful@gmail.com>
2026-06-12 16:58:59 -0300

    ending the day customizing message from the tutorial

--- hello.py
+++ hello.py
@@ -1,15 +1,35 @@
 from loguru import logger
+import sys 
+
+
+# # classic message - debug
+# logger.debug('debug message')
 
 
 
-# classic message - debug
-logger.debug('debug message')
+# # log - for info message
+# logger.info('info message')
+
+# # log - for error
+# logger.error('ERROR MESSAGE')
 
 
 
-# log - for info message
-logger.info('info message')
+# # adding log level
+# logger.remove()
 
-# log - for error
-logger.error('ERROR MESSAGE')
+# logger.add(sys.stderr, level="TRACE")
 
+
+# logger.trace("Will this be visible?")
+
+# logger.debug("A debug message")
+
+# logger.info("An info message")
+
+
+
+# customizing message
+logger.remove()
+logger.add(sys.stderr,format="{message}")
+logger.info('simple message')
